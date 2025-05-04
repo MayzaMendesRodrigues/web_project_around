a5c2e85709c217578902c06fa5a080e2ec76d6ba
Mayza <mayzamrodrigues@gmail.com>
2025-05-04 17:10:49 -0300

    refactor: implement template element and JS manipulation for existing popup

--- scripts/index.js
+++ scripts/index.js
@@ -6,9 +6,15 @@ const editAboutMe = document.querySelector("#popup__aboutMe")
 const profileName = document.querySelector(".profile__name")
 const profileAboutMe = document.querySelector(".profile__aboutMe")
 const like = document.querySelector(".cards__like")
-const popup = document.getElementById("popup")
+const popupContainer = document.getElementById("popup")
 const cardsContainer = document.getElementById("cards__content")
 const cardsTemplate = document.getElementById("cards__template")
+const popupTemplate = document.getElementById("popup__template")
+const editProfile = document.getElementById("profile__edit-btn")
+const addCard = document.getElementById("profile__add_card-btn")
+
+
+
 
 
 const cards = [
@@ -38,6 +44,65 @@ const cards = [
   }
 ]
 
+const popupContent = [
+  {
+    title: "Editar perfil",
+    placeholderFirst: "Nome",
+    placeholderLast: "About Me",
+    buttonPopup: "Salvar"
+  },
+  {
+    title: "Novo Local",
+    placeholderFirst: "Título",
+    placeholderLast: "Link de imagem",
+    buttonPopup: "Criar"
+  }
+]
+
+function createPopUp(index){
+popupContainer.innerHTML = ""
+const popupData = popupContent[index]
+const clonePopup = popupTemplate.content.cloneNode(true)
+const titlePopup = clonePopup.querySelector(".popup__title")
+const inputFirst = clonePopup.querySelector("#popup__placeholderFirst");
+const inputLast = clonePopup.querySelector("#popup__placeholderLast");
+const saveBtn = clonePopup.querySelector(".popup__save");
+const closeBtn = clonePopup.querySelector(".popup__closed")
+
+  titlePopup.textContent = popupData.title
+  inputFirst.placeholder = popupData.placeholderFirst
+  inputLast.placeholder = popupData.placeholderLast
+  saveBtn.textContent = popupData.buttonPopup
+
+if(index === 0){
+  inputFirst.value = profileName.textContent
+  inputLast.value = profileAboutMe.textContent
+
+  saveBtn.addEventListener("click", () => {
+    profileName.textContent = inputFirst.value
+    profileAboutMe.textContent = inputLast.value
+
+    closePopupContent()
+    })
+} else {
+
+}
+closeBtn.addEventListener("click", closePopupContent)
+
+
+popupContainer.append(clonePopup)
+popupContainer.style.display='flex';
+}
+
+editProfile.addEventListener("click", () => createPopUp(0))
+addCard.addEventListener("click", () => createPopUp(1))
+
+
+function closePopupContent(){
+  document.getElementById('popup').style.display='none';
+}
+
+
 cards.forEach(card => {
   const clone = cardsTemplate.content.cloneNode(true)
 
@@ -47,29 +112,6 @@ cards.forEach(card => {
   cardsContainer.appendChild(clone)
   console.log(clone)
 })
-
-function openPopupContent(){
-  popup.style.display='flex';
-
-  editName.value = profileName.textContent
-  editAboutMe.value = profileAboutMe.textContent
-}
-openPopup.addEventListener("click", openPopupContent)
-
-
-function closePopupContent(){
-  document.getElementById('popup').style.display='none';
-}
-closePopup.addEventListener("click", closePopupContent)
-
-
-saveEdit.addEventListener("click", function() {
-profileName.textContent = editName.value
-profileAboutMe.textContent = editAboutMe.value
-
-closePopupContent()
-})
-
 
 
 
