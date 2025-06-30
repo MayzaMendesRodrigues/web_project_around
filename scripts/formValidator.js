cdb3c565c94768902ebde74e8329df7c5c37701c
Mayza <mayzamrodrigues@gmail.com>
2025-06-30 08:45:47 -0300

    fix: correct text styling in card component

--- scripts/formValidator.js
+++ scripts/formValidator.js
@@ -1,4 +1,3 @@
-
 export class FormValidator {
   constructor(config, formElement) {
     this._config = config;
@@ -20,11 +19,6 @@ export class FormValidator {
     errorElement.classList.add(this._config.errorClass);
   }
 
-//     const showInputError = (formElement, inputElement, errorMessage, config) => {
-// //     const formError = formElement.querySelector(`#${inputElement.id}-error`);
-// //     formError.textContent = errorMessage;
-// //     formError.classList.add(config.errorClass);
-// //   };
 
 
   _hideInputError(inputElement) {
@@ -36,12 +30,6 @@ export class FormValidator {
     errorElement.textContent = "";
   }
 
-  //   const hideInputError = (formElement, inputElement, config) => {
-//     const formError = formElement.querySelector(`#${inputElement.id}-error`);
-//     inputElement.classList.remove(config.inputErrorClass);
-//     formError.classList.remove(config.errorClass);
-//     formError.textContent = "";
-//   };
 
   _checkInputValidity(inputElement) {
     if (!inputElement.validity.valid) {
@@ -51,31 +39,13 @@ export class FormValidator {
     }
   }
 
- //   const isValid = (formElement, inputElement, config) => {
-//     if (!inputElement.validity.valid) {
-//       // Se NÃO (!), mostre o elemento de erro.
-//       showInputError(
-//         formElement,
-//         inputElement,
-//         inputElement.validationMessage,
-//         config
-//       );
-//     } else {
-//       // Se for válido, oculte o elemento de erro.
-//       hideInputError(formElement, inputElement, config);
-//     }
-//   };
+
 
   _hasInvalidInput() {
     return this._inputList.some((inputElement) => !inputElement.validity.valid);
   }
 
 
-//   const hasInvalidInput = (inputList) => {
-//     return inputList.some((inputElement) => {
-//       return !inputElement.validity.valid;
-//     });
-//   };
 
 
   _toggleButtonState() {
@@ -88,15 +58,6 @@ export class FormValidator {
     }
   }
 
-//    const toggleButtonState = (inputList, buttonElement, config) => {
-// //     if (hasInvalidInput(inputList)) {
-// //       buttonElement.classList.add(config.inactiveButtonClass);
-// //       buttonElement.setAttribute("disabled", "");
-// //     } else {
-// //       buttonElement.classList.remove(config.inactiveButtonClass);
-// //       buttonElement.removeAttribute("disabled");
-// //     }
-// //   };
 
   _setEventListeners() {
     this._toggleButtonState();
@@ -110,23 +71,7 @@ export class FormValidator {
   }
 
 
-//   const setEventListener = (formElement, config) => {
-//     const inputList = Array.from(
-//       formElement.querySelectorAll(config.inputSelector)
-//     );
-//     const buttonElement = formElement.querySelector(
-//       config.submitButtonSelector
-//     );
 
-//     toggleButtonState(inputList, buttonElement, config);
-
-//     inputList.forEach((inputElement) => {
-//       inputElement.addEventListener("input", () => {
-//         isValid(formElement, inputElement, config);
-//         toggleButtonState(inputList, buttonElement, config);
-//       });
-//     });
-//   };
   enableValidation() {
     this._formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
@@ -138,33 +83,3 @@ export class FormValidator {
     this._setEventListeners();
   }
 }
-//   const enableValidation = (config) => {
-
-//     const formList = Array.from(document.querySelectorAll(config.formSelector));
-//     formList.forEach((formElement) => {
-//       formElement.addEventListener("submit", (evt) => {
-//         evt.preventDefault();
-//         const inputList = Array.from(
-//           formElement.querySelectorAll(config.inputSelector)
-//         );
-
-//         inputList.forEach((inputElement) => {
-//           isValid(formElement, inputElement, config);
-//                       console.log(inputList)
-
-//         });
-//       });
-//       setEventListener(formElement, config);
-//     });
-
-//   };
-
-//   enableValidation({
-//     formSelector: ".popup__form",
-//     inputSelector: ".popup__input",
-//     submitButtonSelector: ".popup__save",
-//     inactiveButtonClass: "popup__save_inactive",
-//     inputErrorClass: "popup__input_type_error",
-//     errorClass: "popup__input-error_active",
-//   });
-// }
